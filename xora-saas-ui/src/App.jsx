6885a40949eb7928c19f8d1e3bb4a64073bb9936
ratalla816 <rob.atalla@robatalla816.com>
2024-10-01 21:09:27 -0500

    added components

--- xora-saas-ui/src/App.jsx
+++ xora-saas-ui/src/App.jsx
@@ -1,29 +1,13 @@
-// import React from 'react'
-
-// export default 
-// const App = () => {
-//   return (
-//     <div>
-//     <h1 className="text-3xl font-bold underline">
-//       Hello world!
-//     </h1>
-//     </div>
-//   )
-// }
-
-// export default App
 
 
-
-
-// import Header from "./sections/Header.jsx";
-// import Hero from "./sections/Hero.jsx";
-// import Features from "./sections/Features.jsx";
-// import Pricing from "./sections/Pricing.jsx";
-// import Faq from "./sections/Faq.jsx";
-// import Testimonials from "./sections/Testimonials.jsx";
-// import Download from "./sections/Download.jsx";
-// import Footer from "./sections/Footer.jsx";
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
+import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
+import Download from "./sections/Download.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
   return (
